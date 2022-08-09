43407b06869dc67b64bb3a41131f3552c02c0985
Ihsan Khan <77566263+codequail@users.noreply.github.com>
2022-08-09 18:16:57 +0500

    change loading animatio

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
     // Show loading icon inside element identified by 'selector'.
     var showLoading = function (selector) {
       var html = "<div class='text-center'>";
-      html += "<img src='images/loading.gif'></div>";
+      html += "<img src='images/loading.svg'><br><br></div>";
       insertHtml(selector, html);
     };
     
